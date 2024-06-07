2344df239b6bbede275d419b6312d585917d4c8e
boburastonov <boburastonov33@gmail.com>
2024-06-07 19:45:33 +0500

    Add hero content without image optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/hero";
 import Product from "@/components/product";
 import { ProductType } from "@/interface";
 
@@ -5,8 +6,8 @@ export default async function Home() {
   const res = await fetch("https://fakestoreapi.com/products");
   const products: ProductType[] = await res.json();
   return (
-    
-    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-52">
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0">
+      <Hero />
       <section className="flex flex-col space-y-12">
         <h1 className="text-5xl font-bold text-center">DREAM SHOP DEALS</h1>
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">

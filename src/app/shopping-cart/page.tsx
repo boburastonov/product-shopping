2e7ac378788fba6f2a3e15e72d78722b6e9cb31e
boburastonov <boburastonov33@gmail.com>
2024-06-13 22:05:37 +0500

    Add product count(increment and decrement) functions

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -4,10 +4,45 @@ import CustomImage from "@/components/image";
 import { ProductType } from "@/interface";
 import { StarIcon as StarIconOutline } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const ShoppingCart = () => {
-  const products: ProductType[] =
-    JSON.parse(localStorage.getItem("carts") as string) || [];
+  const [products, setProducts] = useState<ProductType[]>(
+    JSON.parse(localStorage.getItem("carts") as string) || []
+  );
+
+  const removeProduct = (id: number) => {
+    const newProducts = products.filter((product) => product.id !== id);
+    localStorage.setItem("carts", JSON.stringify(newProducts));
+    setProducts(newProducts);
+  };
+
+  const handleIncrement = (id: number) => {
+    const newProducts = products.map((product) => {
+      if (product.id === id) {
+        return { ...product, quantity: product.quantity + 1 };
+      }
+      return product;
+    });
+    localStorage.setItem("carts", JSON.stringify(newProducts));
+    setProducts(newProducts);
+  };
+
+  const handleDecrement = (id: number) => {
+    const existProduct = products.find((product) => product.id === id);
+    if (existProduct?.quantity == 1) {
+      removeProduct(existProduct.id);
+    } else {
+      const newProducts = products.map((product) => {
+        if (product.id === id) {
+          return { ...product, quantity: product.quantity - 1 };
+        }
+        return product;
+      });
+      localStorage.setItem("carts", JSON.stringify(newProducts));
+      setProducts(newProducts);
+    }
+  };
 
   return (
     <div className="h-screen bg-gray-100 pt-20">
@@ -61,7 +96,10 @@ const ShoppingCart = () => {
                 </div>
                 <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                   <div className="flex items-center border-gray-100">
-                    <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    <span
+                      className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                      onClick={() => handleDecrement(product.id)}
+                    >
                       {" "}
                       -{" "}
                     </span>
@@ -71,7 +109,10 @@ const ShoppingCart = () => {
                       value={product.quantity}
                       min="1"
                     />
-                    <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    <span
+                      className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                      onClick={() => handleIncrement(product.id)}
+                    >
                       {" "}
                       +{" "}
                     </span>
@@ -90,6 +131,7 @@ const ShoppingCart = () => {
                       stroke-width="1.5"
                       stroke="currentColor"
                       className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                      onClick={() => removeProduct(product.id)}
                     >
                       <path
                         stroke-linecap="round"

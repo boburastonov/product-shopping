f7870bc71a42159d935a0542292cc9693e80baea
boburastonov <boburastonov33@gmail.com>
2024-06-13 23:34:18 +0500

    Add Cta component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Cta from "@/components/cta";
 import Hero from "@/components/hero";
 import Product from "@/components/product";
 import { ProductType } from "@/interface";
@@ -9,13 +10,13 @@ export default async function Home() {
     <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0">
       <Hero />
       <section className="flex flex-col space-y-12">
-        <h1 className="text-5xl font-bold text-center">DREAM SHOP DEALS</h1>
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <Product key={product.id} product={product} />
           ))}
         </div>
       </section>
+      <Cta />
     </main>
   );
 }

3d679917651aec52d7c2c8bff0ec4f3f289cffeb
boburastonov <boburastonov33@gmail.com>
2024-06-13 23:36:08 +0500

    Update navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,6 +18,9 @@ const Navbar = () => {
           <Link href={"/products"} className="mr-5 hover:text-gray-900">
             Products
           </Link>
+          <Link href={"/contacts"} className="mr-5 hover:text-gray-900">
+            Contacts
+          </Link>
         </nav>
         <Link href={'/shopping-cart'}>
           <button className="button bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">

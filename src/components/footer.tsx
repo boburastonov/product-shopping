37b62d737436bc4789c6d57c5455482ee76797aa
boburastonov <boburastonov33@gmail.com>
2024-08-21 10:16:00 +0500

    Update svg styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,8 +23,8 @@ const Footer = () => {
           <a className="text-gray-500">
             <svg
               fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               stroke-width="2"
               className="w-5 h-5"
               viewBox="0 0 24 24"
@@ -35,8 +35,8 @@ const Footer = () => {
           <a className="ml-3 text-gray-500">
             <svg
               fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               stroke-width="2"
               className="w-5 h-5"
               viewBox="0 0 24 24"
@@ -48,8 +48,8 @@ const Footer = () => {
             <svg
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               stroke-width="2"
               className="w-5 h-5"
               viewBox="0 0 24 24"
@@ -62,8 +62,8 @@ const Footer = () => {
             <svg
               fill="currentColor"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               stroke-width="0"
               className="w-5 h-5"
               viewBox="0 0 24 24"
